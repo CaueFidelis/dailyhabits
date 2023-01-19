09a66249d40f750adae7c8c888f0affe285c573b
Caue <cauefidelis@live.com>
2023-01-19 18:40:39 -0300

    Creating get route summary

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,3 +1,4 @@
+import { DomHandlerOptions } from './../../mobile/node_modules/domhandler/lib/esm/index.d';
 import { FastifyInstance } from 'fastify';
 import { z } from 'zod';
 import { prisma } from './lib/prisma';
@@ -122,5 +123,32 @@ export async function appRoutes(app: FastifyInstance) {
         },
       });
     }
+  });
+
+  app.get('/summary', async (request) => {
+    const summary = await prisma.$queryRaw`
+      SELECT 
+        D.id,
+        D.date,
+        (
+          SELECT
+            cast(count(*) as float)
+          FROM day_habits DH
+          WHERE DH.day_id = D.id
+        ) as completed,
+        (
+          SELECT
+            cast(count(*) as float)
+          FROM habit_week_days HWD
+          JOIN habits H
+            ON H.id = HWD.habit_id
+          WHERE
+            HWD.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int)
+            AND H.created_at <= D.date
+        ) as amount
+      FROM days D
+    `;
+
+    return summary;
   });
 }
