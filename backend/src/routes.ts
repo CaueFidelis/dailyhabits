217fc759c14d61f311302d01301aa1d51623b654
Caue <cauefidelis@live.com>
2023-01-19 17:48:38 -0300

    Creating route for toggle habits

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -70,4 +70,57 @@ export async function appRoutes(app: FastifyInstance) {
       },
     });
   });
+
+  app.patch('/habits/:id/toggle', async (request) => {
+    const toggleHabitParams = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = toggleHabitParams.parse(request.params);
+
+    const today = dayjs().startOf('day').toDate();
+
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today,
+      },
+    });
+
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today,
+        },
+      });
+    }
+
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      },
+    });
+
+    if (dayHabit) {
+      // remove
+      await prisma.dayHabit.delete({
+        where: {
+          day_id_habit_id: {
+            day_id: day.id,
+            habit_id: id,
+          },
+        },
+      });
+    } else {
+      // Complete
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      });
+    }
+  });
 }
