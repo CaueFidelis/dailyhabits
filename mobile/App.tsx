bea09053e0f38010d52e734c0f4b7b4e29569d63
Caue <cauefidelis@live.com>
2023-01-22 19:40:37 -0300

    Creating new pages and installing navigation in app

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -7,7 +7,7 @@ import {
   Inter_800ExtraBold,
 } from '@expo-google-fonts/inter';
 import { Loading } from './src/components/Loading';
-import { Home } from './src/screens/Home';
+import { Routes } from './src/routes';
 import './src/lib/dayjs';
 
 export default function App() {
@@ -22,7 +22,7 @@ export default function App() {
 
   return (
     <>
-      <Home />
+      <Routes />
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
